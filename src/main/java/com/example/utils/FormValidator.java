e5163529843d35df14d2d13b43d024844c44d5e4
Jakub Tucek <jakub.tucek@gmail.com>
2016-09-07 16:12:08 +0200

    Add example registration

---
+++ src/main/java/com/example/utils/FormValidator.java
@@ -0,0 +1,18 @@
+package com.example.utils;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Jakub Tucek on 07.09.2016.
+ */
+@Service
+public class FormValidator {
+
+    public boolean areStringsValid(String... parameters) {
+        for (String p : parameters) {
+            if (p == null || p.matches("^\\s*$")) return false;
+        }
+        return true;
+    }
+
+}
---
+++ src/main/java/com/example/view/RegistrationBean.java
@@ -0,0 +1,55 @@
+package com.example.view;
+
+import com.example.utils.FormValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Jakub Tucek on 07.09.2016.
+ */
+@Component
+@Scope("session")
+public class RegistrationBean {
+    private String firstName, lastName, emailAddress;
+
+    private final FormValidator formValidator;
+
+    @Autowired
+    public RegistrationBean(FormValidator formValidator) {
+        this.formValidator = formValidator;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String doRegistration() {
+        if (formValidator.areStringsValid(firstName,lastName, emailAddress)) {
+            return "registration-success";
+        } else {
+            return "registration-error";
+        }
+    }
+}
